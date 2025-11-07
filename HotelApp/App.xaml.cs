8d611965239263b77be2a06365d891b61429257a
peque <peque@DELL>
2025-11-07 00:03:35 -0300

    Adicionar arquivos de projeto.

---
+++ HotelApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace HotelApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
