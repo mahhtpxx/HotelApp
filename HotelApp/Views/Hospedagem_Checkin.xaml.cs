2fe590b7903d5b5cdba8d0e76643efe9373f0c46
mahhtpx <maelianacleto19@gmail.com>
2025-11-14 23:32:24 -0300

    sunday = humilation

--- HotelApp/Views/Hospedagem_Checkin.xaml.cs
+++ HotelApp/Views/Hospedagem_Checkin.xaml.cs
@@ -2,9 +2,22 @@ namespace HotelApp.Views
 {
     public partial class Hospedagem_Checkin : ContentPage
     {
+        App PropriedadesApp;
+
+
         public Hospedagem_Checkin()
         {
             InitializeComponent();
+
+            PropriedadesApp = (App)Application.Current;
+
+            Suíte.ItemsSource = PropriedadesApp.lista_quartos;
+
+            dt_check_out.MinimumDate = DateTime.Now;
+            dt_check_out.MaximumDate = new DateTime(DateTime.Now.Year, DateTime.Now.Month + 1, DateTime.Now.Day);
+
+            dt_check_out.MinimumDate = dtof_checkin.Date.AddDays(1);
+            dt_check_out.MaximumDate = dtof_checkin.Date.AddMonths(6);
         }
 
 
@@ -20,6 +33,15 @@ namespace HotelApp.Views
         }
 
 
+        private void dtpck_checkin_DateSelected(object sender, DateChangedEventArgs e)
+        {
+            DatePicker elemento = sender as DatePicker;
+
+            DateTime data_selecionada_checkin = elemento.Date;
+
+            dt_check_out.MinimumDate = data_selecionada_checkin.AddDays(1);
+            dt_check_out.MaximumDate = data_selecionada_checkin.AddMonths(6);
+        }
 
     }
 }
